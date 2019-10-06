9fde4acc37c8230f3cdda7634a81085b3090f10f
shon <shon.sanches@gmail.com>
2019-10-06 21:26:46 +0100

    Updates

---
+++ precache-manifest.1a2a76e3cd8414b49c5951d51aed023c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb97a9f0a12fe00f51160d9dc7a1acb6",
+    "url": "/react-tetris/index.html"
+  },
+  {
+    "revision": "7b332311fe048fe3249b",
+    "url": "/react-tetris/static/css/main.6b7ee09b.chunk.css"
+  },
+  {
+    "revision": "44fc5a1cade4277dc0fb",
+    "url": "/react-tetris/static/js/2.52f5e320.chunk.js"
+  },
+  {
+    "revision": "7b332311fe048fe3249b",
+    "url": "/react-tetris/static/js/main.6ef9fe29.chunk.js"
+  },
+  {
+    "revision": "c3212cc9c94e2446d3ba",
+    "url": "/react-tetris/static/js/runtime-main.2b6c7b46.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/react-tetris/static/media/bg.2810fe6b.png"
+  }
+]);
