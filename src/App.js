4367b8c918a9f88d2fef4b312f37952812ea04c4
shon <shon.sanches@gmail.com>
2019-10-06 21:18:43 +0100

    clear rows  drop user interval

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Tetris from './components/Tetris';
 
 const App = () => (
-  <div className='App'>
-    <Tetris/>
+  <div className="App">
+    <Tetris />
   </div>
 );
+
 export default App;
--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { StyledStage } from './styles/StyledStage';
+
 import Cell from './Cell';
-import { StyledCell } from './styles/StyledCell';
 
 const Stage = ({ stage }) => (
   <StyledStage width={stage[0].length} height={stage.length}>
     {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} />))}
-    </StyledStage>
+  </StyledStage>
 );
 
 export default Stage;
